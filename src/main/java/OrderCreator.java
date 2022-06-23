78819ac6574f88d4d61cfb79eca7defeaf57f7d6
witney <valentinareinande@gmail.com>
2022-06-22 22:36:45 -0300

    First Commit

--- src/main/java/OrderCreator.java
+++ src/main/java/OrderCreator.java
@@ -10,8 +10,8 @@ import java.util.Random;
 
 public class OrderCreator {
     public static void main(String[] args) {
-        String queueUrl = "https://sqs.us-east-1.amazonaws.com/451619603272/order-queue";
-        BasicSessionCredentials awsCreds = new BasicSessionCredentials("ASIAWSJVAINEPU5YLD7I", "a4/cT961ikCDzpIay3h1waPg34+plPtIvso/j+AL", "FwoGZXIvYXdzEGsaDMGxPDnCPO2LsAQwAiLGAbbOsSvfgBNW6zMwSgDiicPQzekf2KONOj5BbyiQ8LoKbjAvtii2+/5IfH6EDmlNULdL86kfK+aOfboaujWDCP7yHhzVFEWf6ozFwrj0C3r4kgPhw02AUMWc/D7eVySPEiS3TpqF3opZ5gfNXPSYERbbLEdq5SNOSswdMajXti0nOMYzEV3MCSGId0PpFMtEUDZEX1WpAnwCgexE9YH6h2qSNWQAhKw3yUI6wLjn5HBZN/cA1hLt1MxzRnavEB8nuQVi1t3XMyjBypiVBjItWOCtJO5BHniBi83yJHNp8o27hNiqq9F3469YT8zKSP+73aL6NQ9irpxC19No");
+        String queueUrl = "https://sqs.us-east-1.amazonaws.com/638915226488/order-queue";
+        BasicSessionCredentials awsCreds = new BasicSessionCredentials("ASIAZJQSQVN4BMXN6ABX", "e8LQGzAEqgkxFy21lTNR0tnmPYFw6tAMQM/K/r4a", "FwoGZXIvYXdzEGIaDH8R32Q5ZT4OGlff5iLQAfNnpjiz0ICoZI9qc1vQ5IQFIsrRnebQjNzN9W1iQp8SWzPB87Yemhp2tVHDjQdayx62pFqK0AStkmZdqBu+1o6vQM/pux8dN2SKXTHDDaozJQqCbcHRDjBe95hmXmpt9R2d/lyZpp0w7PcDX1weW/iCzcKrijN9n4nJY2LXFPOtYzAVQmnqPiebYm5uMNOSqcIE16244wSfvOUvkSNFoNAo4gRF8OJ39JmN92YmJby/+wKBr/skDxn5gxjsfP4sqYFWeVK0Wai5K3rmh0j/3pgooerOlQYyLS+fndx7u7uryAKdFdsUvnSaZp2S0KPYgGX5SFkvO3aWxNCzfpoqoboCD1yFQw==");
         AmazonSQS sqs = AmazonSQSClient.builder().withRegion(Regions.DEFAULT_REGION).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
         int orderNum = 1;
         Random random = new Random();
