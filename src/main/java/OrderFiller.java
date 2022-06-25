57b737256b69b3f99e8bb8e25158029650f6a652
witney <valentinareinande@gmail.com>
2022-06-25 13:55:03 -0300

    Second Commit

--- src/main/java/OrderFiller.java
+++ src/main/java/OrderFiller.java
@@ -12,7 +12,7 @@ import com.amazonaws.services.sqs.model.Message;
 public class OrderFiller {
     public static void main(String[] args) {
         String queueUrl = "https://sqs.us-east-1.amazonaws.com/638915226488/order-queue";
-        BasicSessionCredentials awsCreds = new BasicSessionCredentials("ASIAZJQSQVN4BMXN6ABX", "e8LQGzAEqgkxFy21lTNR0tnmPYFw6tAMQM/K/r4a", "FwoGZXIvYXdzEGIaDH8R32Q5ZT4OGlff5iLQAfNnpjiz0ICoZI9qc1vQ5IQFIsrRnebQjNzN9W1iQp8SWzPB87Yemhp2tVHDjQdayx62pFqK0AStkmZdqBu+1o6vQM/pux8dN2SKXTHDDaozJQqCbcHRDjBe95hmXmpt9R2d/lyZpp0w7PcDX1weW/iCzcKrijN9n4nJY2LXFPOtYzAVQmnqPiebYm5uMNOSqcIE16244wSfvOUvkSNFoNAo4gRF8OJ39JmN92YmJby/+wKBr/skDxn5gxjsfP4sqYFWeVK0Wai5K3rmh0j/3pgooerOlQYyLS+fndx7u7uryAKdFdsUvnSaZp2S0KPYgGX5SFkvO3aWxNCzfpoqoboCD1yFQw==");
+        BasicSessionCredentials awsCreds = new BasicSessionCredentials("!!!!!!", "!!!!!", "!!!");
         AmazonSQS sqs = AmazonSQSClient.builder().withRegion(Regions.DEFAULT_REGION).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
 
         while (true) {
